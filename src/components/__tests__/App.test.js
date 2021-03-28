d2c0fd4a9f60b2b94374fb943a161d571d6cd158
Graeme Chalmers <chalmers.graeme@gmail.com>
2021-03-27 23:11:53 -0700

    added second test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import { shallow } from 'enzyme';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
 it('shows a comment box', () => {
-  const div = document.createElement('div');
+  const wrapped = shallow(<App />);
 
-  ReactDOM.render(<App />, div);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
 
-  expect(div.innerHTML).toContain('Comment Box');
+it('shows a comment list', () => {
+  const wrapped = shallow(<App />);
 
-  ReactDOM.unmountComponentAtNode(div);
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
