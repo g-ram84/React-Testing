d548735c6bc137545f70e7c380d15107cd8425b4
Graeme Chalmers <chalmers.graeme@gmail.com>
2021-03-29 15:56:47 -0700

    Set up header

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
-import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from 'actions/types';
 import axios from 'axios'
 
 export function saveComment(comment) {
   return {
     type: SAVE_COMMENT,
     payload: comment
-  }
+  };
 }
 
 export function fetchComments() {
@@ -14,5 +14,12 @@ export function fetchComments() {
   return {
     type: FETCH_COMMENTS,
     payload: response
-  }
+  };
+}
+
+export function changeAuth(isLoggedIn) {
+  return {
+    type: CHANGE_AUTH,
+    payload: isLoggedIn
+  };
 }
